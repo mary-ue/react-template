958a6b977c842da4e651e627876b528773571f4c
mary-ue <maryandreeva100@gmail.com>
2023-09-06 17:56:25 +0300

    start project

---
+++ src/module.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import cat from './img/cat.jpg'
+
+const Module = () => {
+  return (
+    <>
+      <h1>Module</h1>
+      <img src={cat} width={500} />
+    </>
+  )
+};
+
+export default Module;
